7cbe9b79c9c01bdc7d38f0b4138a1cc7004c04a4
younj1601 <younj1601@wrdsb.ca>
2020-10-06 15:42:35 +0000

    Finished the fourth task for 2.03

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ *Calculate the compound interest based on the users info
+ * @author Jackson
  */
 public class Main {
 
@@ -9,7 +10,26 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
-    
+    //scanner for input
+    Scanner input = new Scanner(System.in);
+
+    //Get all of the information from the user
+    System.out.println("Please enter a starting balance");
+    double balance = input.nextDouble();
+
+    System.out.println("Please enter your interest rate");
+    double interest = input.nextDouble();
+
+    System.out.println("Please enter the number of years you will invest for");
+    int years = input.nextInt();
+
+    //get the interest into a decimal place
+    interest = interest*0.01;
+
+    //for loop to calculate how much the user will gain
+    for(int count = 1; count <= years; count++){
+      balance = balance + interest*balance;
+    }
+    System.out.println("Your final balance would be $" + balance);
   }
 }
